4cbcf0869c5c2b47fa2572ca3585481bb94e0462
harithajith <harithajith@gmail.com>
2022-07-08 15:24:11 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,17 +1,23 @@
-import os
-import mlflow
+from sahi.model import Yolov5DetectionModel
 
-from random import random
+from azure.storage.blob import BlobServiceClient
 
-# define functions
-def main():
-    mlflow.log_param("hello_param", "world")
-    mlflow.log_metric("hello_metric", random())
-    os.system(f"echo 'hello world' > helloworld.txt")
-    mlflow.log_artifact("helloworld.txt")
+STORAGEACCOUNTURL = "https://testws2273653885.blob.core.windows.net"
+STORAGEACCOUNTKEY = "2TQq7sVVNaGld4i2jmMzuXbuxh5ILOcF42Vu9CjZRAuiWDsAQB/awXFTDOplc6fZCiuL9TMiOhpj+AStwQmJkA=="
+CONTAINERNAME = "azureml"
+BLOBNAME = "best299.pt"
 
+blob_service_client_instance = BlobServiceClient(
+    account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
 
-# run functions
-if __name__ == "__main__":
-    # run main function
-    main()
+blob_client_instance = blob_service_client_instance.get_blob_client(
+    CONTAINERNAME, BLOBNAME, snapshot=None)
+
+blob_data = blob_client_instance.download_blob()
+image_path = "./test.png"
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+detection_model = Yolov5DetectionModel(model_path="best299.pt",confidence_threshold=0.8,device="cpu")
+result = get_sliced_prediction(image,detection_model,slice_height = 416,slice_width = 416, overlap_height_ratio = 0.6, overlap_width_ratio = 0.6)
+result.export_visuals(export_dir="demo_data_src/")
