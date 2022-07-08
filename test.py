3164bce6948720cf3cceb8cc73c013f84ac2835f
harithajith <harithajith@gmail.com>
2022-07-08 15:23:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,9 @@ blob_client_instance = blob_service_client_instance.get_blob_client(
     CONTAINERNAME, BLOBNAME, snapshot=None)
 
 blob_data = blob_client_instance.download_blob()
-
+image_path = "./test.png"
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 detection_model = Yolov5DetectionModel(model_path="best299.pt",confidence_threshold=0.8,device="cpu")
 result = get_sliced_prediction(image,detection_model,slice_height = 416,slice_width = 416, overlap_height_ratio = 0.6, overlap_width_ratio = 0.6)
