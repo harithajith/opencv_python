c3a3b12b27121e80d17ac4696fbeddd5db23b81a
harithajith <harithajith@gmail.com>
2022-07-08 15:45:53 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-from sahi.model import Yolov5DetectionModel
+'''from sahi.model import Yolov5DetectionModel'''
 '''
 from azure.storage.blob import BlobServiceClient
 
@@ -13,7 +13,7 @@ blob_service_client_instance = BlobServiceClient(
 blob_client_instance = blob_service_client_instance.get_blob_client(
     CONTAINERNAME, BLOBNAME, snapshot=None)
 
-blob_data = blob_client_instance.download_blob()'''
+blob_data = blob_client_instance.download_blob()''''''
 image_path = "./test.png"
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -21,3 +21,5 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 detection_model = Yolov5DetectionModel(model_path="best299.pt",confidence_threshold=0.8,device="cpu")
 result = get_sliced_prediction(image,detection_model,slice_height = 416,slice_width = 416, overlap_height_ratio = 0.6, overlap_width_ratio = 0.6)
 result.export_visuals(export_dir="demo_data_src/")
+'''
+print("Hellow ")
